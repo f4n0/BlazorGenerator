c8f891b56d4bbb635f37debf6e7c697bc7023131
stefanoPrimo <36199751+stefanoPrimo@users.noreply.github.com>
2021-11-12 17:39:24 +0100

    New Version + Added Dependecies

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -12,8 +12,18 @@ Write-Host "Output folder is '$OutputFolder'"
 if (Test-Path $OutputFolder) { Remove-Item $OutputFolder -Force -Recurse }
 
 Write-Host "$spacer Restoring solution $spacer"
-dotnet restore "$ProjectRoot\Eos.BlazorGenerator.sln" --configfile nuget.config 
+dotnet restore "$ProjectRoot\Eos.BlazorGenerator.sln" --configfile "$ProjectRoot\nuget.config"
 if ($LASTEXITCODE -ne 0) { throw "Failed" }
+
+Write-Host "$spacer Increase Build Number $spacer"
+[xml]$csprojcontents = Get-Content -Path "$ProjectRoot\Eos.Blazor.Generator\Eos.Blazor.Generator.csproj"
+$versionNo = [version] $csprojcontents.Project.PropertyGroup[0].Version
+$newVersion = "{0}.{1}.{2}" -f $versionNo.Major, $versionNo.Minor, ($versionNo.Build+1)
+Write-Host $newVersion
+$csprojcontents.Project.PropertyGroup[0].Version = $newVersion
+$csprojcontents.Save("$ProjectRoot\Eos.Blazor.Generator\Eos.Blazor.Generator.csproj")
+if ($LASTEXITCODE -ne 0) { throw "Failed" }
+
 
 Write-Host "$spacer Building solution $spacer"
 dotnet build "$ProjectRoot\Eos.BlazorGenerator.sln" -c Release
