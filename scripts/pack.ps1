4e2ccf98c472632b205b6ef9e915e0ba5c055c4a
Thomas Torggler <thomas.torggler@eos-solutions.it>
2021-12-22 17:07:55 +0100

    Updated pack script for rename

--- scripts/pack.ps1
+++ scripts/pack.ps1
@@ -12,21 +12,21 @@ Write-Host "Output folder is '$OutputFolder'"
 if (Test-Path $OutputFolder) { Remove-Item $OutputFolder -Force -Recurse }
 
 Write-Host "$spacer Restoring solution $spacer"
-dotnet restore "$ProjectRoot\Eos.BlazorGenerator.sln" --configfile "$ProjectRoot\nuget.config"
+dotnet restore "$ProjectRoot\BlazorGenerator.sln" --configfile "$ProjectRoot\nuget.config"
 if ($LASTEXITCODE -ne 0) { throw "Failed" }
 
 Write-Host "$spacer Increase Build Number $spacer"
-[xml]$csprojcontents = Get-Content -Path "$ProjectRoot\Eos.Blazor.Generator\Eos.Blazor.Generator.csproj"
+[xml]$csprojcontents = Get-Content -Path "$ProjectRoot\BlazorGenerator\BlazorGenerator.csproj"
 $versionNo = [version] $csprojcontents.Project.PropertyGroup[0].Version
 $newVersion = "{0}.{1}.{2}" -f $versionNo.Major, $versionNo.Minor, ($versionNo.Build+1)
 Write-Host $newVersion
 $csprojcontents.Project.PropertyGroup[0].Version = $newVersion
-$csprojcontents.Save("$ProjectRoot\Eos.Blazor.Generator\Eos.Blazor.Generator.csproj")
+$csprojcontents.Save("$ProjectRoot\BlazorGenerator\BlazorGenerator.csproj")
 if ($LASTEXITCODE -ne 0) { throw "Failed" }
 
 
 Write-Host "$spacer Building solution $spacer"
-dotnet build "$ProjectRoot\Eos.BlazorGenerator.sln" -c Release
+dotnet build "$ProjectRoot\BlazorGenerator.sln" -c Release
 if ($LASTEXITCODE -ne 0) { throw "Failed" }
 
 Write-Host "$spacer Packing solution $spacer" 
