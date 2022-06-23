f6816cf054ba5b989ab4c04e5a1483996e8e9326
stefanoPrimo <stefanoprimo13@gmail.com>
2022-06-23 16:55:09 +0200

    Now Available for WebAssemblies (and MAUI)

---
+++ TestWebAssembly/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using TestWebAssembly;
+using BlazorGenerator;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddBlazorGen();
+
+await builder.Build().RunAsync();
