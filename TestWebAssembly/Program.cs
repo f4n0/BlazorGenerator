a2844e7d66cc15bff1e6bd597af58f34d4c863ea
stefanoPrimo <stefanoprimo13@gmail.com>
2022-11-28 11:36:11 +0100

    net7

--- TestWebAssembly/Program.cs
+++ TestWebAssembly/Program.cs
@@ -4,10 +4,11 @@ using TestWebAssembly;
 using BlazorGenerator;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+//builder.RootComponents.Add<App>("#app");
+//builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddBlazorGen();
+
+builder.Services.AddBlazorGen(options => options.ShowBreaddcrumbs = true);
 
 await builder.Build().RunAsync();
