7e00eb28e04e03bd68bd57b40ade0964220f017f
stefanoPrimo <stefanoprimo13@gmail.com>
2023-06-19 17:16:03 +0200

    add customizable NavMenu footer

---
+++ BlazorGenerator/Attributes/Subform - Copy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorGenerator.Attributes
+{
+  [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+  public class MenuFooter : Attribute
+  {
+    public MenuFooter()
+    {
+    }
+  }
+}
