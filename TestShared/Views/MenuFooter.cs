c5aacde6cec7186cf48bf0b715e3fd6d0b384770
stefanoPrimo <stefanoprimo13@gmail.com>
2024-05-07 10:01:54 +0200

    menu order sequence

---
+++ TestShared/Views/MenuFooter.cs
@@ -0,0 +1,15 @@
+﻿using BlazorGenerator.Attributes;
+using Microsoft.FluentUI.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestShared.Views
+{
+  [AddToMenu(Title = "Issue?", Route = "https://github.com/EOS-Solutions/Gordon/issues", Icon = typeof(Icons.Regular.Size16.Archive), OrderSequence = 9999)]
+  public class MenuFooter
+  {
+  }
+}
