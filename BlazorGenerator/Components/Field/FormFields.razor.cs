6c4f04ab4cfbea97778e43d13a4a3567517c7442
stefanoPrimo <stefanoprimo13@gmail.com>
2024-05-21 11:38:28 +0200

    Fix OnLookup

--- BlazorGenerator/Components/Field/FormFields.razor.cs
+++ BlazorGenerator/Components/Field/FormFields.razor.cs
@@ -31,12 +31,12 @@ namespace BlazorGenerator.Components.Field
       var color = Field.Color?.Invoke(Data);
       if (color != null)
         styles += "color: " + color.ToAttributeValue() + ";";
+
       commonAttributes = new()
       {
         { "Id", Id },
         {"Appearance",FluentInputAppearance.Filled },
-        {"ReadOnly", !Editable },
-        {"role", "password" },
+        {"ReadOnly", !Editable || (Field.OnLookup != null) },
         {"style", styles },
         {"Immediate", Field.Immediate }
       };
@@ -44,6 +44,14 @@ namespace BlazorGenerator.Components.Field
       HasLookup = Field.OnLookup != null;
 
       return base.OnParametersSetAsync();
+    }
+
+    void GenericOnClick()
+    {
+      if (HasLookup)
+      {
+        LookupOpen = true;
+      }
     }
   }
 }
